799e7c9c7d2283be1c0d796396b9739d90a18f42
Antonio Moreno <ammz@deversorius.net>
2021-09-12 01:30:33 +0200

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+/**
+ * Author: Antonio Moreno (ammz@deversorius.net) 
+ * Date: 11/09/2021
+ * 
+ * A simple "Hello World!" program in Java
+ * 
+ * @author ammz
+ *
+ */
+public class Hello {
+
+	public static void main(String args[]) {
+		System.out.println("Antonio Moreno");
+	}
+}
