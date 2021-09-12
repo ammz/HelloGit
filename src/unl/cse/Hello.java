c30a2a3f0b939fab46deff1e040edbeecac0098c
Antonio Moreno <ammz@deversorius.net>
2021-09-12 02:13:27 +0200

    incluye el paquete unl/cse

---
+++ src/unl/cse/Hello.java
@@ -0,0 +1,19 @@
+/**
+ * Author: Antonio Moreno (ammz@deversorius.net) 
+ * Date: 11/09/2021
+ * 
+ * A simple "Hello World!" program in Java
+ * 
+ * @author ammz
+ *
+ */
+package unl.cse;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Antonio Moreno");
+		System.out.println("Computer Science");
+		System.out.println("Informática");
+	}
+}
